488fa2a22eb25de1bd94fb4aa59388e0ac6e003c
midnightbr <80632969+midnightbr@users.noreply.github.com>
2021-04-18 18:10:50 -0300

    Aulas de Java Básico

---
+++ java/aulas/PrimeiroPrograma/src/main/java/cursoemvideo/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package cursoemvideo.primeiroprograma;
+/**
+ *
+ * @author Marcos Henrique
+ */
+public class PrimeiroPrograma {
+    
+    public static void main(String[] args) {
+       System.out.print("Olá Mundo!");
+    }
+    
+}
